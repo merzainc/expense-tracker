a15f4d1b3f1c6b0770454483c9b26bf6ca15b056
merzainc <h200649z@hit.ac.zw>
2023-03-28 15:07:53 +0200

    Introduce state for expenses.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
+import { useState } from 'react';
 import ExpenseList from './components/ExpenseList';
 
-const expenses = [
+interface Expense {
+  id: number;
+  desc: string;
+  amount: number;
+  category: string;
+}
+
+/** initial state object */
+const initialExpenses = [
   { id: 1, desc: 'Rent', amount: 75, category: 'Utilities' },
   { id: 2, desc: 'Joger Pants', amount: 15, category: 'Clothing' },
-  { id: 3, desc: 'Samon and Curry', amount: 75, category: 'Foods' },
 ];
 
 function App() {
-  return (
-    <ExpenseList
-      expenses={expenses}
-      onRemove={(expense) => console.log(expense)}
-    />
-  );
+  const [expenses, setExpenses] = useState(initialExpenses);
+
+  const removeExpense = (id: number) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
+
+  return <ExpenseList expenses={expenses} onRemove={removeExpense} />;
 }
 
 export default App;

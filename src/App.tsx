70f64458eac58aa6f26a6fcc5943d5efe2343750
merzainc <h200649z@hit.ac.zw>
2023-03-28 14:48:00 +0200

    Test ExpenseList component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import ExpenseList from './components/ExpenseList';
 
+const expenses = [
+  { id: 1, desc: 'Rent', amount: 75, category: 'Utilities' },
+  { id: 2, desc: 'Joger Pants', amount: 15, category: 'Clothing' },
+  { id: 3, desc: 'Samon and Curry', amount: 75, category: 'Foods' },
+];
+
 function App() {
-  return <ExpenseList />;
+  return (
+    <ExpenseList
+      expenses={expenses}
+      onRemove={(expense) => console.log(expense)}
+    />
+  );
 }
 
 export default App;

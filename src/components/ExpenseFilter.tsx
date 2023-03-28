2a0e52407268098ad889983886b22a8ec18cd458
merzainc <h200649z@hit.ac.zw>
2023-03-28 15:18:43 +0200

    Define ExpenseFilter component.

---
+++ src/components/ExpenseFilter.tsx
@@ -0,0 +1,12 @@
+const ExpenseFilter = () => {
+  return (
+    <select name='' id='' className='form-select mb-3'>
+      <option value=''>All categories</option>
+      <option value='Groceries'>Groceries</option>
+      <option value='Utilities'>Utilities</option>
+      <option value='Entertainment'>Entertainment</option>
+    </select>
+  );
+};
+
+export default ExpenseFilter;

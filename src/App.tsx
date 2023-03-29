d434ff7cae1fb0371ddf67e9988a69bb4c7fc9ce
merzainc <h200649z@hit.ac.zw>
2023-03-29 08:57:52 +0200

    Refactor code implementation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
 import { useState } from 'react';
-import ExpenseFilter from './components/ExpenseFilter';
+import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
+import ExpenseFilter from './components/ExpenseFilter';
 
 /** initial state object */
 const initialExpenses = [
-  { id: 1, desc: 'Rent', amount: 75, category: 'Utilities' },
+  { id: 1, desc: 'Drink 5L', amount: 3.5, category: 'Groceries' },
   { id: 2, desc: 'Joger Pants', amount: 15, category: 'Clothing' },
+  { id: 3, desc: 'Plumbing', amount: 20, category: 'Utilities' },
 ];
 
 function App() {
   const [expenses, setExpenses] = useState(initialExpenses);
+  const [selectedCategory, setCategory] = useState('');
+
+  const summary = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   const removeExpense = (id: number) => {
     setExpenses(expenses.filter((expense) => expense.id !== id));
   };
 
-  const handleSelectedCategory = (category: string) => {
-    setExpenses(expenses.filter((e) => e.category === category));
-  };
-
   return (
     <div className='container'>
-      <ExpenseFilter onSelectCategory={handleSelectedCategory} />
-      <ExpenseList expenses={expenses} onRemove={removeExpense} />
+      <h3 className='text-center mt-2 text-secondary'>Expense Tracker</h3>
+      <ExpenseForm />
+      <ExpenseFilter onSelectCategory={(category) => setCategory(category)} />
+      <ExpenseList expenses={summary} onRemove={removeExpense} />
     </div>
   );
 }
---
+++ src/categories.ts
@@ -0,0 +1,4 @@
+/** expense categories */
+const categories = ['Utilities', 'Clothing', 'Entertainment', 'Groceries'];
+
+export default categories;
